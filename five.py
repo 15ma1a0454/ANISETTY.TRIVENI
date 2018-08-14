5be58ee0c215a394307da71de980a88706dfb925
15ma1a0454 <41770024+15ma1a0454@users.noreply.github.com>
2018-08-14 11:52:18 -0400

    Create five.py

---
+++ five.py
@@ -0,0 +1,12 @@
+p=int(input())
+q=int(input())
+r=int(input())
+if (p>=q) and (p>=r):
+        num1=p
+        print(num1)
+elif (q>=p) and (q>=r):
+        num2=q
+        print(num2)
+else:
+  num3=r
+  print(num3)
