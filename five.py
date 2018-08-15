3f2ae952e9544f6cf1d835f972e5136d2aac10e8
15ma1a0454 <41770024+15ma1a0454@users.noreply.github.com>
2018-08-15 01:41:35 -0400

    Update five.py

--- five.py
+++ five.py
@@ -1,12 +1,12 @@
 p=int(input())
 q=int(input())
 r=int(input())
-if (p>=q) and (p>=r):
+if(p>=q) and (p>=r):
         num1=p
         print(num1)
-elif (q>=p) and (q>=r):
+elif(q>=p) and (q>=r):
         num2=q
-        print(num2)
+       print(num2)
 else:
   num3=r
   print(num3)
