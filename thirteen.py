8eec17331be1970149dce06a0d99c0ac310c27e4
15ma1a0454 <41770024+15ma1a0454@users.noreply.github.com>
2018-08-28 02:30:46 -0400

    Update thirteen.py

--- thirteen.py
+++ thirteen.py
@@ -3,7 +3,10 @@ if(r<=1000):
    for i in (2,r):
        t=r%i
        if(t==0):
-          print('yes')
+          print('no')
           break
+       else:
+         print('yes')
+         break
 else:
    print('no')
